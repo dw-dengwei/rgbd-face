a7442c2e279db91c8d94fb31f9f3768c35d114c9
Wei Deng <dw-dengwei@outlook.com>
2022-11-25 14:38:37 +0800

    Refactor: move all config to config file

--- config/config.py
+++ config/config.py
@@ -12,6 +12,9 @@ class BaseConfig:
     batch_size = 512
     learning_rate = 3e-4
     valid_check_interval = 0.5
+    momentum = 0.9
+    weight_decay = 0.0005
+    warmup_ratio = 0.05
 
     """model"""
     num_classes = 600
@@ -21,16 +24,32 @@ class BaseConfig:
     out_features = 1024
     reduction = 16
     lr_reduce_epoch = [6, 10, 17]
+    alpha = 1
+    beta = 2
+    gamma = 3
+    lambda_1 = 0.5
+    lambda_2 = 0.05
+    rgb_in_channels = 3
+    depth_in_channels = 3
 
-    """data"""
+    """training data"""
     train_data_root = "/home/dw/vgg"
-    train_rgb_dir = "crop"
+    train_rgb_dir = "aligned"
     train_dzyx_dir = "depth_normal"
     train_ids = 468
     train_using_modal = ("rgb", "normal")
+    using_test = 'lock3dface'
 
-    valid_data_root = "/home/dw/rgbd/Texas"
-    valid_rgb_dir = "aligned"
-    valid_dzyx_dir = "depth_normal"
-    valid_ids = 118
-    valid_using_modal = train_using_modal
+    """Texas data"""
+    texas_data_root = "/home/dw/rgbd/Texas"
+    texas_rgb_dir = "aligned"
+    texas_dzyx_dir = "depth_normal"
+    texas_ids = 118
+    texas_using_modal = train_using_modal
+
+    """Lock3DFace data"""
+    lock3dface_datainfo = "/home/dw/code/rgbd-face/util/lock3dface_info.csv"
+    lock3dface_data_root = "/home/dw/data/lock3dface/test/"
+    lock3dface_rgb_dir = "aligned"
+    lock3dface_dzyx_dir = "depth"
+    lock3dface_using_modal = train_using_modal
