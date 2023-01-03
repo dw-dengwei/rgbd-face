3243bfd78f5ccb2e3354a9b0def35f320079fad7
Wei Deng <dw-dengwei@outlook.com>
2023-01-03 13:41:11 +0800

    Add: segment lmdb reader

--- dataset/vgg.py
+++ dataset/vgg.py
@@ -1,8 +1,10 @@
 from torchvision import transforms as trans
 from torch.utils.data import Dataset
+from config.config import BaseConfig as config
 
 import numpy as np
 import torch
+import lmdb
 import cv2
 
 
@@ -11,16 +13,33 @@ class Vgg(Dataset):
         self,
         rgb_path,
         dzyx_path,
+        seg_path,
+        seg_lmdb_path,
         transform,
         using_modal=("rgb", "depth")
     ):
         super(Vgg, self).__init__()
         self.rgb_path = rgb_path
         self.dzyx_path = dzyx_path
+        self.seg_path = seg_path
+        self.seg_lmdb_path = seg_lmdb_path
         self.transform = transform
         self.using_modal = using_modal
+        self.random_patch_prob = config.random_patch_prob
+        self.random_patch_size = config.random_patch_size
+        self.atts = ['background', 'skin', 'l_brow', 'r_brow', 'l_eye', 'r_eye',
+                     'eye_g', 'l_ear', 'r_ear',
+                     'ear_r', 'nose', 'mouth', 'u_lip', 'l_lip', 'neck',
+                     'neck_l',
+                     'cloth', 'hair', 'hat']
+        self.seg_channel = [
+            0, 1, 2, 3, 4, 5, 6, 10, 11, 12, 13
+        ]
+        self.env = None
+        self.cnt_get = 0
 
-        assert len(self.rgb_path) == len(self.dzyx_path), (len(self.rgb_path), len(self.dzyx_path))
+        assert len(self.rgb_path) == len(self.dzyx_path), (
+        len(self.rgb_path), len(self.dzyx_path))
         assert len(using_modal) > 1
 
     def __len__(self):
@@ -51,7 +70,7 @@ class Vgg(Dataset):
         # if show, comment
         normal *= 255
         # cv2.imwrite("normal.png", normal[:, :, ::-1])
-        return normal[:,:,::-1]
+        return normal[:, :, ::-1]
 
     @staticmethod
     def random_patch(rgb, dzyx, prob=0.2, patch_size=30):
@@ -75,38 +94,72 @@ class Vgg(Dataset):
         return rgb, dzyx
 
     def __getitem__(self, index):
+        if self.env is None:
+            self.env = lmdb.open(
+                self.seg_lmdb_path,
+                readonly=True, lock=False, readahead=False, meminit=False
+            )
+
         label = torch.tensor(int(self.rgb_path[index].split("/")[-2]) - 1)
         out = []
         rgb = cv2.imread(self.rgb_path[index])
         dzyx = cv2.imread(self.dzyx_path[index], -1)
-        rgb, dzyx = Vgg.random_patch(rgb, dzyx, prob=0, patch_size=30)
+        rgb, dzyx = Vgg.random_patch(
+            rgb, dzyx, prob=self.random_patch_prob,
+            patch_size=self.random_patch_size
+        )
         if "rgb" in self.using_modal:
+            # rgb = torch.from_numpy((rgb.transpose(2, 0, 1)))
             rgb = trans.ToTensor()(rgb)
+            # print(rgb.shape)
             out.append(rgb)
-
         if "depth" in self.using_modal:
             depth = dzyx[:, :, :1]
+            # depth = torch.from_numpy(depth.transpose(2, 0, 1))
             depth = trans.ToTensor()(depth)
             out.append(depth)
         if "normal" in self.using_modal:
             normal = dzyx[:, :, 1:]
+            # normal = torch.from_numpy(normal.transpose(2, 0, 1))
             normal = trans.ToTensor()(normal)
             out.append(normal)
         if "segment" in self.using_modal:
-            # TODO segment input
-            segment = torch.clone(rgb)
+            id_num = self.rgb_path[index].split('/')[-2]
+            file_num = self.rgb_path[index].split('/')[-1][:-4]
+            with self.env.begin(write=False) as txn:
+                buf = txn.get(f'{id_num}_{file_num}'.encode())
+            segment = np.frombuffer(buf, dtype=np.float32).reshape(
+                (11, 128, 128)
+            )
+            segment = torch.from_numpy(segment)
             out.append(segment)
+            # segment = np.load(self.seg_path[index])[self.seg_channel, :, :].transpose(1,2,0)
+            # segment = trans.ToTensor()(segment)
+            # out.append(segment)
 
         return *out, label
 
 
 if __name__ == '__main__':
-    rgb = cv2.imread('/home/dw/vgg/aligned/1/1.jpg')
-    dzyx = cv2.imread('/home/dw/vgg/depth_normal/1/1_dzyx.png')
-    rgb, dzyx = Vgg.random_patch(rgb, dzyx, 1.0)
-    depth = dzyx[:, :, 0]
-    normal = dzyx[:, :, 1:]
-    cv2.imwrite('test_rgb.jpg', rgb)
-    cv2.imwrite('test_dzyx.png', dzyx)
-    cv2.imwrite('test_depth.jpg', depth)
-    cv2.imwrite('test_normal.png', normal)
+    from util.data import get_vgg_rgb_dzyx_seg_path
+    from torch.util.data import DataLoader
+
+    train_dataset = Vgg(
+        *get_vgg_rgb_dzyx_seg_path(
+            config.train_data_root,
+            config.train_rgb_dir,
+            config.train_dzyx_dir,
+            config.train_segment_dir,
+            config.train_ids
+        ),
+        None,
+        config.train_using_modal
+    )
+    loader = DataLoader(
+        train_dataset,
+        batch_size=512,
+        num_workers=8,
+        pin_memory=True,
+        persistent_workers=True,
+        shuffle=True
+    )
