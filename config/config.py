ca1abbd79efc14949612b6764625f8c5d32aeb62
Wei Deng <dw-dengwei@outlook.com>
2023-01-09 14:30:31 +0800

    20230109_V1

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ class BaseConfig:
     """env"""
     gpu = [1, 3]
     precision = 16
-    num_workers = 10
+    num_workers = 4
     pin_memory = True
     persistent_workers = True
     random_seed = 42
@@ -11,7 +11,7 @@ class BaseConfig:
     epoch = 30
     batch_size = 512
     learning_rate = 3e-4
-    valid_check_interval = 0.5
+    valid_check_interval = 1.0
     momentum = 0.9
     weight_decay = 0.0005
     warmup_ratio = 0.05
@@ -36,8 +36,13 @@ class BaseConfig:
     train_data_root = "/home/dw/vgg"
     train_rgb_dir = "aligned"
     train_dzyx_dir = "depth_normal"
+    train_segment_dir = "segment"
+    train_segment_lmdb = "segment/lmdb"
     train_ids = 5000
-    train_using_modal = ("rgb", "normal", "segment")
+    train_using_modal = ("rgb", "normal")
+    random_patch_prob = 0.2
+    random_patch_size = 30
+    random_patch_method = 'improve'
     using_test = 'lock3dface'
     single_modal = 'depth'
 
